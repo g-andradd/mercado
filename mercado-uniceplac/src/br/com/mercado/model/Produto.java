3be54f14477a8eabf5de5c7b4571f6bb3e522cbe
Gabriel Andrade Almeida <gabrielandraad@gmail.com>
2022-05-18 13:57:42 -0300

    Entidade Produto feita

--- mercado-uniceplac/src/br/com/mercado/model/Produto.java
+++ mercado-uniceplac/src/br/com/mercado/model/Produto.java
@@ -1,4 +1,62 @@
 package br.com.mercado.model;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 public class Produto {
+
+    //Atributos do produto
+    private final Long id;
+    private String nome;
+    private String descricao;
+    private BigDecimal preco;
+    private final LocalDate dataFabricacao;
+    private final LocalDate dataVencimento;
+
+    //Método construtor do produto
+    public Produto(Long id, String nome, String descricao, BigDecimal preco, LocalDate dataFabricacao, LocalDate dataVencimento) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.preco = preco;
+        this.dataFabricacao = dataFabricacao;
+        this.dataVencimento = dataVencimento;
+    }
+
+    //Métodos get e set dos atributos do produto
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+
+    public LocalDate getDataFabricacao() {
+        return dataFabricacao;
+    }
+
+    public LocalDate getDataVencimento() {
+        return dataVencimento;
+    }
 }
