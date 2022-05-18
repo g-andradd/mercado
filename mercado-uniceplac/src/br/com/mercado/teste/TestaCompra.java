1c862be78a8f6cf6887cb1886e8effce7ce8e082
Gabriel Andrade Almeida <gabrielandraad@gmail.com>
2022-05-18 16:32:58 -0300

    Refatorei a classe TestaCompra

--- mercado-uniceplac/src/br/com/mercado/teste/TestaCompra.java
+++ mercado-uniceplac/src/br/com/mercado/teste/TestaCompra.java
@@ -10,9 +10,9 @@ import java.time.LocalDate;
 public class TestaCompra {
     public static void main(String[] args) {
 
-        Produto produto = new Produto(1l, "Biscoito Oreo", "Biscoito oreo com 6 biscoitos",
+        Produto produto = new Produto(1L, "Biscoito Oreo", "Biscoito oreo com 6 biscoitos",
                 new BigDecimal("2"), LocalDate.now(), LocalDate.now());
-        CaixaRegistradora caixaRegistradora = new CaixaRegistradora(1l);
+        CaixaRegistradora caixaRegistradora = new CaixaRegistradora(1L);
 
         Compra compra = new Compra(caixaRegistradora);
         compra.setProduto(produto, 5);
