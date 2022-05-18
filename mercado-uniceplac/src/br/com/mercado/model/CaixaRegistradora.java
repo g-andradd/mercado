198d2f1ef465c8ea820f9865b9491919ee22f1bc
Gabriel Andrade Almeida <gabrielandraad@gmail.com>
2022-05-18 16:54:43 -0300

    tirei os comentarios da classe caixaRegistradora

--- mercado-uniceplac/src/br/com/mercado/model/CaixaRegistradora.java
+++ mercado-uniceplac/src/br/com/mercado/model/CaixaRegistradora.java
@@ -3,22 +3,22 @@ package br.com.mercado.model;
 public class CaixaRegistradora {
 
     private final Long id;
-//    private Caixa caixa;
-                    //  , Caixa caixa
-    public CaixaRegistradora(Long id) {
+    private Caixa caixa;
+
+    public CaixaRegistradora(Long id, Caixa caixa) {
         this.id = id;
-//        this.caixa = caixa;
+        this.caixa = caixa;
     }
 
     public Long getId() {
         return id;
     }
 
-//    public Caixa getCaixa() {
-//        return caixa;
-//    }
-//
-//    public void setCaixa(Caixa caixa) {
-//        this.caixa = caixa;
-//    }
+    public Caixa getCaixa() {
+        return caixa;
+    }
+
+    public void setCaixa(Caixa caixa) {
+        this.caixa = caixa;
+    }
 }
