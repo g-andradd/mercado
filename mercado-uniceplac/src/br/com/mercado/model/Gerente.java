a32ad1a8677dbb1b5b2e5ed382d46720e72505fc
GenisonLeal <87821998+GenisonLeal@users.noreply.github.com>
2022-05-21 17:46:39 -0300

    Update Gerente.java

--- mercado-uniceplac/src/br/com/mercado/model/Gerente.java
+++ mercado-uniceplac/src/br/com/mercado/model/Gerente.java
@@ -1,24 +1,5 @@
 package br.com.mercado.model;
 
-public class Gerente extends Funcionario{
-	
-	private int senha;
-	private int login;
-
-	public int getSenha() {
-		return senha;
-	}
-
-	public void setSenha(int senha) {
-		this.senha = senha;
-	}
-
-	public int getLogin() {
-		return login;
-	}
-
-	public void setLogin(int login) {
-		this.login = login;
-	}
+public class Gerente extends Funcionario{	
 	
 }
