afe50d5afc247c7775b1d2fe4c02de6540fadeb5
Gabriel Andrade Almeida <gabrielandraad@gmail.com>
2022-05-21 18:58:00 -0300

    criação do main e testes no testa compra

--- mercado-uniceplac/src/br/com/mercado/teste/TestaCompra.java
+++ mercado-uniceplac/src/br/com/mercado/teste/TestaCompra.java
@@ -1,27 +1,38 @@
 package br.com.mercado.teste;
 
-import br.com.mercado.model.CaixaRegistradora;
-import br.com.mercado.model.Compra;
-import br.com.mercado.model.Produto;
+import br.com.mercado.model.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TestaCompra {
     public static void main(String[] args) {
 
-        Produto produto = new Produto(1L, "Biscoito Oreo", "Biscoito oreo com 6 biscoitos",
+        Produto biscoitoOreo = new Produto(1L, "Biscoito Oreo", "Biscoito oreo com 6 biscoitos",
                 new BigDecimal("2"), LocalDate.now(), LocalDate.now());
-        Produto produto2 = new Produto(1L, "Salgadionho", "salgadinho de requijao",
+        Produto salgadinho = new Produto(1L, "Salgadionho", "salgadinho de requijao",
                 new BigDecimal("3"), LocalDate.now(), LocalDate.now());
+
+        List<RelatorioDeProdutos> relatorioDeProdutos = new ArrayList<>();
+
+        relatorioDeProdutos.add(new RelatorioDeProdutos(biscoitoOreo, 50));
+        relatorioDeProdutos.add(new RelatorioDeProdutos(salgadinho, 50));
+
+        Estoque estoque = new Estoque();
+        estoque.setEstoqueProdutos(relatorioDeProdutos);
+
+        System.out.println(estoque);
+
         CaixaRegistradora caixaRegistradora = new CaixaRegistradora(1L);
 
         Compra compra = new Compra(caixaRegistradora);
-        compra.setProduto(produto, 2);
+        compra.setProduto(biscoitoOreo, 2);
 
         System.out.println(compra.getPreco());
 
-        compra.setProduto(produto2, 2);
+        compra.setProduto(salgadinho, 2);
         System.out.println(compra.getPreco());
         System.out.println(compra.getProdutos());
 
