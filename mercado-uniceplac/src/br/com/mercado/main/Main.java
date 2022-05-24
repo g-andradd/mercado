5e4d2b749756495aeda008b6dbf25205c4abed5f
Guilherme Simões Gonzaga <guilherme.xp.gon@gmail.com>
2022-05-24 12:42:23 -0300

    Comando de Repetição no Main

--- mercado-uniceplac/src/br/com/mercado/main/Main.java
+++ mercado-uniceplac/src/br/com/mercado/main/Main.java
@@ -39,41 +39,67 @@ public class Main {
 		JOptionPane.showMessageDialog(null, " " + estoque, "Produtos do estoque", JOptionPane.PLAIN_MESSAGE);
     
     //Caixa e Caixa Registradora
-      //utilizando o swing para criar um funcionário
-        int opcaoCaixa = JOptionPane.showConfirmDialog(null, "Deseja criar um Caixa?", "Criar um Caixa", JOptionPane.YES_NO_OPTION);
-        if (opcaoCaixa == JOptionPane.YES_OPTION) {
-          JOptionPane.showMessageDialog(null, "Insira as informações a seguir", "Criar um Caixa", JOptionPane.INFORMATION_MESSAGE);
-          String id = JOptionPane.showInputDialog("Insira o id:");
-            String nome = JOptionPane.showInputDialog("Insira o nome:");
-            String cpf = JOptionPane.showInputDialog("Insira o cpf:");
-            String idade = JOptionPane.showInputDialog("Insira a idade:");
-          String salario = JOptionPane.showInputDialog("Insira o salario:");
-
-          //transformar o id em long e o salario em bigdecimal
-          Long idfinal = new Long(id);
-          BigDecimal salariofinal = new BigDecimal(salario);
-
-          //criando um funcionario com os parametros certos
-          Funcionario FunCaixa01 = new Caixa(idfinal, nome, cpf, idade, salariofinal);
-
-          //diálogo final
-          JOptionPane.showMessageDialog(null, "O Funcionário-Caixa, foi criado com sucesso", "Criar um Caixa", JOptionPane.INFORMATION_MESSAGE);
-
-          //operação finalizada caso não queira criar o funcionário caixa
-        } else if (opcaoCaixa == JOptionPane.NO_OPTION) {
-          JOptionPane.showMessageDialog(null, "Operação Finalizada", "Criar um Caixa", JOptionPane.ERROR_MESSAGE);
-        }
-
-        //utilizando o swing para criar a caixa reg.
-        int opcaoCaixaReg = JOptionPane.showConfirmDialog(null, "Deseja criar uma CaixaRegistradora?", "Criar uma Caixa Registradora", JOptionPane.YES_NO_OPTION);
-        if (opcaoCaixaReg == JOptionPane.YES_OPTION){
-          //inserindo 
-          JOptionPane.showMessageDialog(null, "Insira as informações a seguir", "Criar uma Caixa Registradora", JOptionPane.INFORMATION_MESSAGE);
-          String id = JOptionPane.showInputDialog("Insira o id da Caixa Registradora:");
-          Long idfinal = new Long(id);
-
-          CaixaRegistradora CaixaReg01 = new CaixaRegistradora(idfinal);
-        } else if (opcaoCaixaReg == JOptionPane.NO_OPTION) {
-          JOptionPane.showMessageDialog(null, "Operação Finalizada", "Criar um Caixa", JOptionPane.ERROR_MESSAGE);
-        }
+		//variaveis para checar de as informacoes foram inseridas
+		boolean x1 = false;
+		boolean x2 = false;
+		
+		//iniciando o comando de repeticao checando caixa e/ou caixa registradora foram criados
+		do {
+			//se caixa foi preenchido/criado prossegue
+			if(x1 == false) {
+				//utilizando o swing para criar um funcionario
+		        int opcaoCaixa = JOptionPane.showConfirmDialog(null, "Deseja criar um Caixa?", "Criar um Caixa", JOptionPane.YES_NO_OPTION);
+		        if (opcaoCaixa == JOptionPane.YES_OPTION) {
+		          JOptionPane.showMessageDialog(null, "Insira as informacoes a seguir", "Criar um Caixa", JOptionPane.INFORMATION_MESSAGE);
+		          String id = JOptionPane.showInputDialog("Insira o id:");
+		          String nome = JOptionPane.showInputDialog("Insira o nome:");
+		          String cpf = JOptionPane.showInputDialog("Insira o cpf:");
+		          String idade = JOptionPane.showInputDialog("Insira a idade:");
+		          String salario = JOptionPane.showInputDialog("Insira o salario:");
+		
+		          //transformar o id em long e o salario em bigdecimal
+		          Long idfinal = new Long(id);
+		          BigDecimal salariofinal = new BigDecimal(salario);
+		
+		          //criando um funcionario com os parametros certos
+		          Funcionario FunCaixa01 = new Caixa(idfinal, nome, cpf, idade, salariofinal);
+		
+		          //dialogo final
+		          JOptionPane.showMessageDialog(null, "O Funcionario-Caixa, foi criado com sucesso", "Criar um Caixa", JOptionPane.INFORMATION_MESSAGE);
+		          
+		          //confirma a criacao do caixa
+		          x1 = true;
+		          
+		          //operacao finalizada caso nao queira criar o funcionario caixa
+		        } else if (opcaoCaixa == JOptionPane.NO_OPTION) {
+		          JOptionPane.showMessageDialog(null, "Operacao Finalizada", "Criar um Caixa", JOptionPane.ERROR_MESSAGE);
+		        }
+			}
+			
+			//se caixa registradora foi preenchido/criado prossegue
+			if (x2 == false) {
+		        //utilizando o swing para criar a caixa reg.
+		        int opcaoCaixaReg = JOptionPane.showConfirmDialog(null, "Deseja criar uma CaixaRegistradora?", "Criar uma Caixa Registradora", JOptionPane.YES_NO_OPTION);
+		        if (opcaoCaixaReg == JOptionPane.YES_OPTION){
+		          //inserindo 
+		          JOptionPane.showMessageDialog(null, "Insira as informacoes a seguir", "Criar uma Caixa Registradora", JOptionPane.INFORMATION_MESSAGE);
+		          String id = JOptionPane.showInputDialog("Insira o id da Caixa Registradora:");
+		          Long idfinal = new Long(id);
+		
+		          CaixaRegistradora CaixaReg01 = new CaixaRegistradora(idfinal);
+		          //confirma a criacao da caixa registradora
+		          x2 = true;
+		        } else if (opcaoCaixaReg == JOptionPane.NO_OPTION) {
+		          JOptionPane.showMessageDialog(null, "Operacao Finalizada", "Criar um Caixa", JOptionPane.ERROR_MESSAGE);
+		        }
+			}
+		}	
+		while(x1 == false || x2 == false);
 	}
+}
+	
+	
+	
+	
+	
+	
