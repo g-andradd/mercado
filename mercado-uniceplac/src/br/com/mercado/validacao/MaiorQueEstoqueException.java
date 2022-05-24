c0659c20db529b1e4fdd7a90b62ca271d2946ea5
Gabriel Andrade Almeida <gabrielandraad@gmail.com>
2022-05-24 15:28:44 -0300

    arrumando imports

--- mercado-uniceplac/src/br/com/mercado/validacao/MaiorQueEstoqueException.java
+++ mercado-uniceplac/src/br/com/mercado/validacao/MaiorQueEstoqueException.java
@@ -1,6 +1,5 @@
 package br.com.mercado.validacao;
 
-import br.com.mercado.model.Produto;
 import br.com.mercado.model.RelatorioDoProduto;
 
 public class MaiorQueEstoqueException extends RuntimeException{
