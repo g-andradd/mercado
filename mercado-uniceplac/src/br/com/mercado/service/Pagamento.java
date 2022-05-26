b1ac406a932412e4b462cdd60b83b7398acada04
Gabriel Andrade Almeida <gabrielandraad@gmail.com>
2022-05-26 16:22:44 -0300

    Adicionando exception para valor pago menor que preço da compra

--- mercado-uniceplac/src/br/com/mercado/service/Pagamento.java
+++ mercado-uniceplac/src/br/com/mercado/service/Pagamento.java
@@ -1,6 +1,8 @@
 package br.com.mercado.service;
 
 import br.com.mercado.model.Compra;
+import br.com.mercado.validacao.ValorMenorQueACompraException;
+
 import java.math.BigDecimal;
 
 public class Pagamento {
@@ -8,9 +10,9 @@ public class Pagamento {
     public static BigDecimal RecebePagamento(Compra compra, BigDecimal valor) {
         if (valor.doubleValue() > compra.getPreco().doubleValue()) {
             return valor.subtract(compra.getPreco());
+        } else if (valor.doubleValue() < compra.getPreco().doubleValue()) {
+            throw new ValorMenorQueACompraException("O valor da compra deve ser igual ou maior que o preço da compra!!");
         } else {
-            if (valor.doubleValue() < compra.getPreco().doubleValue()) {
-            }
 
             return BigDecimal.ZERO;
         }
---
+++ mercado-uniceplac/src/br/com/mercado/validacao/ValorMenorQueACompraException.java
@@ -0,0 +1,7 @@
+package br.com.mercado.validacao;
+
+public class ValorMenorQueACompraException extends RuntimeException{
+    public ValorMenorQueACompraException(String message) {
+        super(message);
+    }
+}
